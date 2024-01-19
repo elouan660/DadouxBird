8f51453b870a8f78b7b67b454197b082ba0c11a8
elouan6601 <95432031+elouan660@users.noreply.github.com>
2024-01-19 19:02:05 +0100

    php et images3

--- dadoux.gd
+++ dadoux.gd
@@ -1,11 +1,21 @@
 extends Area2D
 
+@export var speed = 400 # How fast the player will move (pixels/sec).
+var screen_size # Size of the game window.
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	print("test")
+	screen_size = get_viewport_rect().size
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	print("toast2")
+	var velocity = Vector2.ZERO # The player's movement vector.
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite2D.play()
+	else:
+		$AnimatedSprite2D.stop()
